93b00acacebea8f730ad8e25a20529ff33154133
William <ywwwtseng@gmail.com>
2025-09-01 23:54:18 +0800

    feat(hooks): useTelegramSDK

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,108 @@
+import { LaunchParamsLike } from '@telegram-apps/transformers';
 
-export {  }
+declare const TELEGRAM_ENV: {
+    MOCK: {
+        launchParams?: (Omit<LaunchParamsLike, 'tgWebAppData'> & {
+            tgWebAppData?: string | URLSearchParams;
+        }) | string | URLSearchParams;
+    };
+    DEFAULT: 0;
+};
+declare function useTelegramSDK(env?: typeof TELEGRAM_ENV[keyof typeof TELEGRAM_ENV]): {
+    launchParams: {
+        tgWebAppBotInline?: boolean;
+        tgWebAppData?: {
+            auth_date: Date;
+            can_send_after?: number;
+            chat?: {
+                id: number;
+                photo_url?: string;
+                type: string;
+                title: string;
+                username?: string;
+            } & {
+                [key: string]: unknown;
+            };
+            chat_type?: string;
+            chat_instance?: string;
+            hash: string;
+            query_id?: string;
+            receiver?: {
+                added_to_attachment_menu?: boolean;
+                allows_write_to_pm?: boolean;
+                first_name: string;
+                id: number;
+                is_bot?: boolean;
+                is_premium?: boolean;
+                last_name?: string;
+                language_code?: string;
+                photo_url?: string;
+                username?: string;
+            } & {
+                [key: string]: unknown;
+            };
+            start_param?: string;
+            signature: string;
+            user?: {
+                added_to_attachment_menu?: boolean;
+                allows_write_to_pm?: boolean;
+                first_name: string;
+                id: number;
+                is_bot?: boolean;
+                is_premium?: boolean;
+                last_name?: string;
+                language_code?: string;
+                photo_url?: string;
+                username?: string;
+            } & {
+                [key: string]: unknown;
+            };
+        } & {
+            [key: string]: unknown;
+        };
+        tgWebAppDefaultColors?: {
+            accent_text_color?: `#${string}`;
+            bg_color?: `#${string}`;
+            button_color?: `#${string}`;
+            button_text_color?: `#${string}`;
+            bottom_bar_bg_color?: `#${string}`;
+            destructive_text_color?: `#${string}`;
+            header_bg_color?: `#${string}`;
+            hint_color?: `#${string}`;
+            link_color?: `#${string}`;
+            secondary_bg_color?: `#${string}`;
+            section_bg_color?: `#${string}`;
+            section_header_text_color?: `#${string}`;
+            section_separator_color?: `#${string}`;
+            subtitle_text_color?: `#${string}`;
+            text_color?: `#${string}`;
+        };
+        tgWebAppFullscreen?: boolean;
+        tgWebAppPlatform: string;
+        tgWebAppShowSettings?: boolean;
+        tgWebAppStartParam?: string;
+        tgWebAppThemeParams: {
+            accent_text_color?: `#${string}`;
+            bg_color?: `#${string}`;
+            button_color?: `#${string}`;
+            button_text_color?: `#${string}`;
+            bottom_bar_bg_color?: `#${string}`;
+            destructive_text_color?: `#${string}`;
+            header_bg_color?: `#${string}`;
+            hint_color?: `#${string}`;
+            link_color?: `#${string}`;
+            secondary_bg_color?: `#${string}`;
+            section_bg_color?: `#${string}`;
+            section_header_text_color?: `#${string}`;
+            section_separator_color?: `#${string}`;
+            subtitle_text_color?: `#${string}`;
+            text_color?: `#${string}`;
+        };
+        tgWebAppVersion: string;
+    } & {
+        [key: string]: unknown;
+    };
+    initDataRaw: string;
+};
+
+export { TELEGRAM_ENV, useTelegramSDK };
---
+++ src/hooks/useTelegramSDK.ts
@@ -0,0 +1,79 @@
+import { useMemo } from 'react';
+import { LaunchParamsLike } from '@telegram-apps/transformers';
+import {
+  mockTelegramEnv,
+  retrieveLaunchParams,
+  retrieveRawInitData,
+  isTMA,
+} from '@telegram-apps/sdk-react';
+
+export const TELEGRAM_ENV: {
+  MOCK: {
+    launchParams?: (Omit<LaunchParamsLike, 'tgWebAppData'> & {
+      tgWebAppData?: string | URLSearchParams;
+    }) | string | URLSearchParams;
+  },
+  DEFAULT: 0
+} = {
+  MOCK: {
+    launchParams: {
+      tgWebAppData: new URLSearchParams([
+        ['user', JSON.stringify({
+          id: 6666666666,
+          first_name: 'T',
+          last_name: 'yw',
+          username: 'ywwwtseng',
+          language_code: 'zh-hans',
+          allows_write_to_pm: true,
+          photo_url: 'https://t.me/i/userpic/320/nQlDMwY_br5G4QK2sd9uK2yC7025mbODcLr8uHJWXX90vnZDywxIOKaH7vXai2FC.svg'
+        })],
+        ['chat_instance', '-5440521606958638813'],
+        ['chat_type', 'sender'],
+        ['auth_date', '1742711181'],
+        ['signature', 'FSFXaPVyWU5py8SyqrrstqPm59esA9zohIyPhn-nKJ9XQS47HeYtw5xnJ4SFy2G2fLFX7GQ5l7H4fxExGif8Aw'],
+        ['hash', 'f2bc216132e681353f74476947e7cbdbc0afd05bbc53c790f829a11a1ac50883']
+      ]),
+      tgWebAppVersion: '8.0',
+      tgWebAppPlatform: 'web',
+      tgWebAppThemeParams: {
+        accent_text_color: '#8774e1',
+        bg_color: '#212121',
+        button_color: '#8774e1',
+        button_text_color: '#ffffff',
+        destructive_text_color: '#ff595a',
+        header_bg_color: '#212121',
+        hint_color: '#aaaaaa',
+        link_color: '#8774e1',
+        secondary_bg_color: '#181818',
+        section_bg_color: '#212121',  
+        section_header_text_color: '#8774e1',
+        subtitle_text_color: '#aaaaaa',
+        text_color: '#ffffff',
+      }
+    }
+  },
+  DEFAULT: 0,
+};
+
+export function useTelegramSDK(env?: typeof TELEGRAM_ENV[keyof typeof TELEGRAM_ENV]) {
+  return useMemo(() => {
+    if (env) {
+      mockTelegramEnv(env);
+    }
+
+    if (isTMA()) {
+      const launchParams = retrieveLaunchParams();
+      const initDataRaw = retrieveRawInitData();
+
+      return {
+        launchParams,
+        initDataRaw,
+      };
+    }
+
+    return {
+      launchParams: null,
+      initDataRaw: null,
+    };
+  }, []);
+}
